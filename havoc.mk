8b923e622074e9657f47d0e3ee5751f1d5727755
Luke Williams <luke@turbox.uk>
2020-01-04 15:15:57 +0000

    Update havoc.mk
    Add me as maintainer
    Set resolution to 1920x1080
    Set havoc build type to GSI

--- havoc.mk
+++ havoc.mk
@@ -1,3 +1,13 @@
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 $(call inherit-product, device/havoc/sepolicy/common/sepolicy.mk)
 -include vendor/havoc/build/core/config.mk
+
+# Havoc Properties
+
+HAVOC_BUILD_TYPE := GSI
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.havoc.maintainer=ExpressLuke
+
